13c01f22dd3827f40ef903001e735393514a0368
daydreamer102 <eiren.mcloughlin@gmail.com>
2016-01-19 15:14:57 +0000

    writing js node code

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 //Just printing to the console
 console.log("hello world");
+
+function add (a, b){
+	var ans = a + b;
+	return ans;
+}
+
+var theAns;
+theAns =  add(5,3);
+console.log("the ans is" + theAns);
+
+//part 2
+
+var name = "John"; //string
+var age = 21; //number
+var male = true; //boolean
+var myArray = new Array(); //creates an array
+if (age == 21){
+	console.log("Your still young");
+}
+
+//part 3
+
